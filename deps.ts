835e553a12d380beb43019f9df8ddde39f981b98
Nassim Zen <hironichu@pm.me>
2024-02-10 16:09:34 +0100

    update Oak version

--- deps.ts
+++ deps.ts
@@ -3,22 +3,22 @@ export {
   assert,
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.202.0/assert/mod.ts";
+} from "https://deno.land/std@0.214.0/assert/mod.ts";
 export {
   afterEach,
   beforeEach,
   describe,
   it,
-} from "https://deno.land/std@0.202.0/testing/bdd.ts";
+} from "https://deno.land/std@0.214.0/testing/bdd.ts";
 
 // Djwt
-export type { Payload } from "https://deno.land/x/djwt@v2.9.1/mod.ts";
+export type { Payload } from "https://deno.land/x/djwt@v3.0.1/mod.ts";
 export {
   create,
   getNumericDate,
   verify,
-} from "https://deno.land/x/djwt@v2.9.1/mod.ts";
-export type { Algorithm } from "https://deno.land/x/djwt@v2.9.1/algorithm.ts";
+} from "https://deno.land/x/djwt@v3.0.1/mod.ts";
+export type { Algorithm } from "https://deno.land/x/djwt@v3.0.1/algorithm.ts";
 
 // Oak
 export type {
@@ -26,10 +26,10 @@ export type {
   Middleware,
   RouterContext,
   RouterMiddleware,
-} from "https://deno.land/x/oak@v12.6.1/mod.ts";
+} from "https://deno.land/x/oak@v13.1.0/mod.ts";
 export {
   Application,
   Context,
   createHttpError,
   Status,
-} from "https://deno.land/x/oak@v12.6.1/mod.ts";
+} from "https://deno.land/x/oak@v13.1.0/mod.ts";
